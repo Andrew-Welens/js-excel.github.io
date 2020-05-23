5068a6dcd314418b5dcdb179d853b7896d815d34
Andrey Zentsov <welens@inbox.ru>
2020-05-23 17:54:46 +0300

    update listeners

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,18 @@
 import {DomLisneter} from './DomLisneter'
 
 export class ExcelComponent extends DomLisneter {
+  constructor($root, options = {}) {
+    super($root, options.listeners)
+    this.name = options.name || ''
+  }
+
+
   // Возвращает шаблон компонента
   toHtml() {
     return ''
+  }
+
+  init() {
+    this.initDOMListeners()
   }
 }
