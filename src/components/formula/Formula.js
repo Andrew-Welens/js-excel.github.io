d58a759be77e6a4da7f425c14e33505fe8d28cac
Andrey Zentsov <welens@inbox.ru>
2020-07-31 17:22:14 +0300

    work-app

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,27 +1,49 @@
 import {ExcelComponent} from '@core/ExcelComponent'
+import {$} from '@core/dom'
 
 export class Formula extends ExcelComponent {
   static className = 'excel__formula'
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
       name: 'Formula',
-      listeners: ['input', 'click']
+      listeners: ['input', 'keydown'],
+      subscribe: ['currentText'],
+      ...options
     })
   }
 
-  toHtml() {
+  toHTML() {
     return `
-       <div class="info">fx</div>
-      <div class="input" contenteditable spellcheck="false"></div>
+      <div class="info">fx</div>
+      <div id="formula" class="input" contenteditable spellcheck="false"></div>
     `
   }
 
-  onInput(event) {
-    console.log(event.type)
+  init() {
+    super.init()
+
+    this.$formula = this.$root.find('#formula')
+
+    this.$on('table:select', $cell => {
+      this.$formula.text($cell.data.value)
+    })
   }
 
-  onClick(event) {
-    console.log(event.type)
+  storeChanged({currentText}) {
+    this.$formula.text(currentText)
+  }
+
+  onInput(event) {
+    const text = $(event.target).text()
+    this.$emit('formula:input', text)
+  }
+
+  onKeydown(event) {
+    const keys = ['Enter', 'Tab']
+    if (keys.includes(event.key)) {
+      event.preventDefault()
+      this.$emit('formula:done')
+    }
   }
 }
