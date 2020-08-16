f1c759a367a102bd4b479a0d9dcc8606b4e836fc
Andrey Zentsov <welens@inbox.ru>
2020-08-16 20:06:10 +0300

    Revert "work-app"
    This reverts commit d58a759be77e6a4da7f425c14e33505fe8d28cac.

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,41 +1,44 @@
-import {createToolbar} from '@/components/toolbar/toolbar.template'
-import {$} from '@core/dom'
-import {ExcelStateComponent} from '@core/ExcelStateComponent'
-import {defaultStyles} from '@/constants'
+import {ExcelComponent} from '@core/ExcelComponent'
 
-export class Toolbar extends ExcelStateComponent {
+export class Toolbar extends ExcelComponent {
   static className = 'excel__toolbar'
 
-  constructor($root, options) {
+  constructor($root) {
     super($root, {
       name: 'Toolbar',
-      listeners: ['click'],
-      subscribe: ['currentStyles'],
-      ...options
+      listeners: ['click']
     })
   }
 
-  prepare() {
-    this.initState(defaultStyles)
-  }
+  toHtml() {
+    return `
+          <div class="button">
+        <i class="material-icons">format_align_left</i>
+      </div>
 
-  get template() {
-    return createToolbar(this.state)
-  }
+      <div class="button">
+        <i class="material-icons">format_align_center</i>
+      </div>
 
-  toHTML() {
-    return this.template
-  }
+      <div class="button">
+        <i class="material-icons">format_align_right</i>
+      </div>
 
-  storeChanged(changes) {
-    this.setState(changes.currentStyles)
+      <div class="button">
+        <i class="material-icons">format_bold</i>
+      </div>
+
+      <div class="button">
+        <i class="material-icons">format_italic</i>
+      </div>
+
+      <div class="button">
+        <i class="material-icons">format_underlined</i>
+      </div>
+    `
   }
 
   onClick(event) {
-    const $target = $(event.target)
-    if ($target.data.type === 'button') {
-      const value = JSON.parse($target.data.value)
-      this.$emit('toolbar:applyStyle', value)
-    }
+    console.log(event.target)
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
-const webpack = require('webpack')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
@@ -11,12 +12,17 @@ const isDev = !isProd
 const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
 
 const jsLoaders = () => {
-  const loaders = ['babel-loader']
+  const loaders = [{
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/preset-env'],
+      plugins: ['@babel/plugin-proposal-class-properties']
+    }
+  }]
 
   if (isDev) {
     loaders.push('eslint-loader')
   }
-
   return loaders
 }
 
@@ -37,6 +43,7 @@ module.exports = {
   },
   devtool: isDev ? 'source-map' : false,
   devServer: {
+    watchContentBase: true,
     port: 3000,
     hot: isDev
   },
@@ -49,40 +56,33 @@ module.exports = {
         collapseWhitespace: isProd
       }
     }),
-    new CopyPlugin([
-      {
-        from: path.resolve(__dirname, 'src/favicon.ico'),
-        to: path.resolve(__dirname, 'dist')
-      }
-    ]),
+    new CopyPlugin([{
+      from: path.resolve(__dirname, 'src/favicon.ico'),
+      to: path.resolve(__dirname, 'dist'),
+    }]),
     new MiniCssExtractPlugin({
       filename: filename('css')
     }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
   ],
   module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: isDev,
-              reloadAll: true
-            }
-          },
-          'css-loader',
-          'sass-loader'
-        ],
+    rules: [{
+      test: /\.s[ac]ss$/i,
+      use: [{
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+          hmr: isDev,
+          reloadAll: true
+        }
       },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: jsLoaders()
-      }
-    ]
+      'css-loader',
+      'sass-loader',
+      ],
+    },
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: jsLoaders(),
+    }
+    ],
   }
 }
