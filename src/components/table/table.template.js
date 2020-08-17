e4f6088eeab367008b2b85d4dca98d37a3d4e7e6
Andrey Zentsov <welens@inbox.ru>
2020-08-18 01:04:52 +0300

    refactor table.tempalate, create gen cell

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,49 +1,48 @@
 const charCodes = {
   A: 65,
   Z: 90,
-};
+}
 
-function generateCell() {
+function toCell() {
   return `
-<div class="column">
-
-</div>
-`;
+<div class="cell"></div>
+`
 }
 
 function toColumn(element) {
   return `
 <div class="column">${element}</div>
-`;
+`
 }
 
-function generateRow(data) {
+function generateRow(idx, data) {
   return `
 <div class="row">
-<div class="row-info"></div>
+<div class="row-info">${idx}</div>
 <div class="row-data">${data}</div>
 </div>
-`;
+`
 }
 
 function toChar(_, idx) {
-  return String.fromCharCode(charCodes.A + idx);
+  return String.fromCharCode(charCodes.A + idx)
 }
 
 export function generateTable(row = 15) {
-  const colCount = charCodes.Z - charCodes.A + 1;
-  const rows = [];
+  const colCount = charCodes.Z - charCodes.A + 1
+  const rows = []
 
   const cols = new Array(colCount).fill('').
       map(toChar).
       map(toColumn).
-      join('');
+      join('')
 
-  rows.push(generateRow(cols));
+  rows.push(generateRow('', cols))
 
   for (let i = 0; i < row; i++) {
-    rows.push(generateRow());
+    const cells = new Array(colCount).fill('').map(toCell).join('')
+    rows.push(generateRow(i + 1, cells))
   }
 
-  return rows.join('');
+  return rows.join('')
 }
