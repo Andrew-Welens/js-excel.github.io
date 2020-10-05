70d2b1f5dbed65487386d73d3791d533ecd37007
Andrey Zentsov <welens@inbox.ru>
2020-10-05 12:59:10 +0300

    feat: debounce for app

--- src/index.js
+++ src/index.js
@@ -6,14 +6,16 @@ import {Formula} from '@/components/formula/Formula'
 import {Table} from '@/components/table/Table'
 import {createStore} from '@core/createStore'
 import {rootReducer} from '@/store/rootReducer'
-import {locStorage} from '@core/utils'
+import {locStorage, debounce} from '@core/utils'
 import {initialState} from '@/store/initialState'
 
 const store = createStore(rootReducer, initialState)
 
-store.subscribe(state => {
+const stateListener = debounce(state => {
   locStorage('excel-state', state)
-})
+}, 350)
+
+store.subscribe(stateListener)
 
 const excel = new Excel('#app', {
   components: [Header, Toolbar, Formula, Table],
